94bf6309656ebac7252ce48542ec160da8e9cd6c
Alan Wood <WoodAlanM@gmail.com>
2024-01-13 13:13:59 -0500

    Possibly finished assignment part 1

--- Data Structures and Algorithms/Assignment/Main.java
+++ Data Structures and Algorithms/Assignment/Main.java
@@ -56,8 +56,12 @@ public class Main {
                 }
             }
         }
-    }
+    }// End bubbleSort
+
     public static void main(String[] args){
+        // Initialize scanner for accepting command line input.
+        Scanner scanner = new Scanner(System.in);
+        
         // Populate States and Capitals array
         String[][] stateCapitalArray = {
             {"Alabama","Alaska","Arizona","Arkansas","California",
@@ -65,16 +69,16 @@ public class Main {
             "Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas",
             "Kentucky","Louisiana","Maine","Maryland","Massachusetts",
             "Michigan","Minnesota","Mississippi","Missouri","Montana",
-            "Nebraska","Nevada","NewHampshire","NewJersey","NewMexico",
+            "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico",
             "New York","North Carolina","North Dakota","Ohio","Oklahoma",
             "Oregon","Pennsylvania","Rhode Island","South Carolina",
-            "SouthDakota","Tennessee","Texas","Utah","Vermont",
+            "South Dakota","Tennessee","Texas","Utah","Vermont",
             "Virginia","Washington","West Virginia","Wisconsin","Wyoming"},
             {"Montgomery","Juneau","Phoenix","Little Rock","Sacramento",
             "Denver","Hartford","Dover","Tallahassee","Atlanta",
-            "Honolulu","Boise","Springfield","Indianapolis","DesMoines",
+            "Honolulu","Boise","Springfield","Indianapolis","Des Moines",
             "Topeka","Frankfort","Baton Rouge","Augusta","Annapolis",
-            "Boston","Lansing","SaintPaul","Jackson","Jefferson City",
+            "Boston","Lansing","Saint Paul","Jackson","Jefferson City",
             "Helena","Lincoln","Carson City","Concord","Trenton",
             "Santa Fe","Albany","Raleigh","Bismarck","Columbus",
             "Oklahoma City","Salem","Harrisburg","Providence","Columbia",
@@ -82,19 +86,54 @@ public class Main {
             "Richmond","Olympia","Charleston","Madison","Cheyenne"}
         };
         
+        // Display a prompt so the user knows what to expect
+        System.out.println("This is a program to test your knowledge of United States Capitals. \n");
+        System.out.println("First, you will be shown a list of tates and their \ncorresponding capitals to refresh your memory. \n");
+        System.out.println("To continue, press ENTER.");
+
+        // Accept an input
+        String readString = scanner.nextLine();
+
+        Boolean enterNotPressed = true;
+
+        while (enterNotPressed == true){
+            if (readString.isEmpty()){
+                enterNotPressed = false;
+            }
+        }
+
         // Display the array for the user ie. each pair of State and Capital's
         for (int i = 0; i < 50; i++){
             System.out.println("State: " + stateCapitalArray[0][i].toString() + ", Capital: " + stateCapitalArray[1][i].toString());
         }
 
+        // Sort the array by capitals.
         bubbleSort(stateCapitalArray);
 
-        // Display the SORTED array for the user.
-        // for (int i = 0; i < 50; i++){
-        //     System.out.println("State: " + stateCapitalArray[0][i].toString() + ", Capital: " + stateCapitalArray[1][i].toString());
-        // }
-        // This seems to work... Start over here!!!!!
+        Boolean notCycled = true;
+        int stateCount = 0;
+        int correctCount = 0;
+
+        System.out.println("\n\nPress ENTER after each entry to continue.\n\n");
+
+        // Begin the capital quizzing portion
+        while (notCycled == true){
+            if (stateCount < 50){
+                System.out.println(String.format("What is the capital of %s? ", stateCapitalArray[0][stateCount].toString()));
+                String inputCapital = scanner.nextLine();
+                String currentCapital = stateCapitalArray[1][stateCount].toString();
+                if (inputCapital.toLowerCase().compareTo(currentCapital.toLowerCase()) == 0){
+                    correctCount++;
+                }
+                stateCount++;
+            }else {
+                notCycled = false;
+            }
+        }// End While
+
+        // After the quizzing cycle is completed, display number correct and exit.
+        System.out.println(String.format("\n\nGreat job!  You got %d correct.\n\n", correctCount));
     }
-};
+};// Main
 
 
